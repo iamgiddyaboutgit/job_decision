269747648a1f9fd74063dd5e3499146ffa58f28d
iamgiddyaboutgit <justinwitherlypatterson@gmail.com>
2024-07-14 17:58:49 -0600

    Add hazard function for JustinDistribution_2.

--- util.py
+++ util.py
@@ -4,6 +4,8 @@
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
+import numpyro.distributions.util as util
+import jax.numpy as jnp
 
 
 def get_sr_weights(n:int) -> np.ndarray:
@@ -61,3 +63,241 @@ class Pert(dist.Beta):
         concentration1 = alpha
         concentration0 = beta
         super().__init__(concentration1, concentration0)
+
+
+class JustinDistribution(dist.Distribution):
+    """Class for modeling time-to-failure."""
+    arg_constraints = {
+        "y_0": dist.constraints.nonnegative,
+        "t_1": dist.constraints.positive,
+        "peak": dist.constraints.nonnegative,
+        "t_2": dist.constraints.positive,
+        "k_2": dist.constraints.less_than(0),
+        "c": dist.constraints.nonnegative
+    }
+    # We put that the support is positive even though
+    # it is really all non-negative real numbers.
+    # I don't think it makes a difference.
+    support = dist.constraints.positive
+    def __init__(self, y_0, k_1, t_1, peak, t_2, k_2, c):
+        """
+        Args:
+            y_0: 0 <= y_0
+            k_1: any non-zero real number. k_1*t_1 < 25.
+            t_1: t_1 > 0. k_1*t_1 < 25.
+            peak: 0 <= peak
+            t_2: t_1 <= t_2
+            k_2: k_2 < 0
+            c: c >= 0
+        """
+        promoted_shapes = util.promote_shapes(y_0, k_1, t_1, peak, t_2, k_2, c)
+        y_0 = promoted_shapes[0]
+        k_1 = promoted_shapes[1]
+        t_1 = promoted_shapes[2]
+        peak = promoted_shapes[3]
+        t_2 = promoted_shapes[4] 
+        k_2 = promoted_shapes[5] 
+        c = promoted_shapes[6]
+
+        # Introduce any other variable that we may want to use later.
+        a_1 = (y_0 - peak)/(1-jnp.exp(k_1 * t_1))
+
+        self.a_1 = a_1
+        
+        # Save others to self.
+        self.y_0 = y_0
+        self.k_1 = k_1
+        self.t_1 = t_1
+        self.peak = peak
+        self.t_2 = t_2
+        self.k_2 = k_2
+        self.c = c
+        
+        super().__init__(batch_shape=jnp.shape(peak), event_shape=())
+
+    def sample(self, key, sample_shape=()):
+        raise NotImplementedError
+    
+    def hazard(self, t):
+        """
+        See: An Introduction to Survival Analysis 
+        Using Stata, Second Edition by Mario Cleves
+        
+        https://books.google.de/books?id=xttbn0a-QR8C&printsec=frontcover&hl=de#v=onepage&q&f=false
+        """
+        a_1 = self.a_1
+        y_0 = self.y_0
+        k_1 = self.k_1
+        t_1 = self.t_1
+        peak = self.peak
+        t_2 = self.t_2 
+        k_2 = self.k_2 
+        c = self.c
+
+        part_0 = jnp.exp(k_1 * t_1)
+        # The hazard function is defined piecewise. 
+        # The pieces are below.
+        h_1 = a_1 * jnp.exp(k_1 * t) + (peak - y_0 * part_0)/(1 - part_0)
+        h_2 = peak
+        h_3 = (peak - c)*jnp.exp(k_2 * (t - t_2)) + c
+
+        h_with_12 = jnp.where(
+            t > t_1,
+            h_2,
+            h_1
+        )
+
+        h_with_123 = jnp.where(
+            t > t_2,
+            h_3,
+            h_with_12
+        )
+
+        return h_with_123
+    
+
+    def cum_haz(self, t):
+        """P(T > t)"""
+        y_0 = self.y_0
+        k_1 = self.k_1
+        t_1 = self.t_1
+        peak = self.peak
+        t_2 = self.t_2 
+        k_2 = self.k_2 
+        c = self.c
+
+        part_0 = jnp.exp(k_1 * t_1)
+
+        cum_haz_1 = ((y_0 - peak)*(jnp.exp(k_1 * t) - 1) + k_1*(peak - y_0*part_0)*t) / ((1 - part_0)*k_1)
+
+        part_1 = ((y_0 - peak)*(part_0 - 1) + k_1*(peak - y_0*part_0)*t_1) / ((1 - part_0)*k_1)
+
+        cum_haz_2 = part_1 + peak * (t - t_1)
+
+        part_2 = part_1 + peak * (t_2 - t_1)
+
+        cum_haz_3 = part_2 + (peak - c)/k_2*jnp.exp(k_2*(t - t_2)) + c*t - (peak - c)/k_2 - c*t_2
+
+        cum_haz_2_with_1 = jnp.where(
+            t > t_1,
+            cum_haz_2,
+            cum_haz_1
+        )
+
+        cum_haz_3_with_21 = jnp.where(
+            t > t_2,
+            cum_haz_3,
+            cum_haz_2_with_1
+        )
+
+        return cum_haz_3_with_21
+    
+
+    def survival(self, t):
+        """P(T > t)"""
+        return jnp.exp(-self.cum_haz(t=t))
+
+    def log_prob(self, t):
+        h = self.hazard(t=t)
+        cum_hazard = self.cum_haz(t=t)
+
+        return jnp.log(h) - cum_hazard
+    
+
+class JustinDistribution_2(dist.Distribution):
+    """Class for modeling time-to-failure."""
+    arg_constraints = {
+        
+    }
+    # We put that the support is positive even though
+    # it is really all non-negative real numbers.
+    # I don't think it makes a difference.
+    support = dist.constraints.nonnegative
+    def __init__(self, ):
+        """
+        """
+        super().__init__(batch_shape=(), event_shape=())
+
+    def sample(self, key, sample_shape=()):
+        raise NotImplementedError
+    
+    def hazard(self, t):
+        num_gompertz_pdfs = 5
+        num_uni_pdfs = 1
+        num_pdfs = num_gompertz_pdfs + num_uni_pdfs
+
+        # Initialize pdf_vals (to be filled later)
+        pdf_vals = np.zeros(shape=(num_pdfs, len(t)))
+
+        # Get the PDF values from multiple PDFs.
+        for i in range(num_gompertz_pdfs):
+            multiple = 7*(i + 1)
+            pdf_vals[i, :] = jnp.exp(
+                dist.Gompertz(
+                    concentration=1.0/np.exp(multiple), 
+                    rate=1
+                ).log_prob(value=t)
+            )
+            next_row = i + 1
+
+        pdf_vals[next_row, :] = jnp.exp(
+            dist.Uniform(
+                low=7, 
+                high=len(t)
+            ).log_prob(value=t)
+        )
+   
+        # tot is calculated for some customized scaling
+        # in the mixture model of all the PDFs.
+        tot = num_pdfs * (num_pdfs + 1)/2
+        weights = (1/tot)*np.arange(num_pdfs, 0, -1)
+        
+        return weights @ pdf_vals
+
+
+    def cum_haz(self, t):
+        """P(T > t)"""
+        y_0 = self.y_0
+        k_1 = self.k_1
+        t_1 = self.t_1
+        peak = self.peak
+        t_2 = self.t_2 
+        k_2 = self.k_2 
+        c = self.c
+
+        part_0 = jnp.exp(k_1 * t_1)
+
+        cum_haz_1 = ((y_0 - peak)*(jnp.exp(k_1 * t) - 1) + k_1*(peak - y_0*part_0)*t) / ((1 - part_0)*k_1)
+
+        part_1 = ((y_0 - peak)*(part_0 - 1) + k_1*(peak - y_0*part_0)*t_1) / ((1 - part_0)*k_1)
+
+        cum_haz_2 = part_1 + peak * (t - t_1)
+
+        part_2 = part_1 + peak * (t_2 - t_1)
+
+        cum_haz_3 = part_2 + (peak - c)/k_2*jnp.exp(k_2*(t - t_2)) + c*t - (peak - c)/k_2 - c*t_2
+
+        cum_haz_2_with_1 = jnp.where(
+            t > t_1,
+            cum_haz_2,
+            cum_haz_1
+        )
+
+        cum_haz_3_with_21 = jnp.where(
+            t > t_2,
+            cum_haz_3,
+            cum_haz_2_with_1
+        )
+
+        return cum_haz_3_with_21
+    
+
+    def survival(self, t):
+        """P(T > t)"""
+        return jnp.exp(-self.cum_haz(t=t))
+
+    def log_prob(self, t):
+        h = self.hazard(t=t)
+        cum_hazard = self.cum_haz(t=t)
+
+        return jnp.log(h) - cum_hazard
